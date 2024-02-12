80b5d41c4504f4453ea48abbabf9e0ff0e45c725
chandan <chan.saikia1884@gmail.com>
2024-02-12 18:23:19 +0530

    Added CrossOrigin annotation UserContrlr

--- src/main/java/com/tradlyf/tradlyf/controller/UserController.java
+++ src/main/java/com/tradlyf/tradlyf/controller/UserController.java
@@ -3,7 +3,7 @@ import com.tradlyf.tradlyf.model.users;
 import com.tradlyf.tradlyf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 public class UserController {
     @Autowired
@@ -12,7 +12,7 @@ public class UserController {
     @ResponseBody
     public users UserLogin(@RequestBody users user1){
         users flag=userService.loginValidation(user1.getEmail(), user1.getPassword());
-        if(flag.getResult()==0){
+        if (flag.getResult() == 0) {
             return flag;
         }
         return flag;
